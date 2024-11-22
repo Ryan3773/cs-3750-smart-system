4940c16cf311b08039c7adc2e81f461fbdc36f4e
Ryan3773 <ryanalexander@mail.weber.edu>
2024-11-21 22:46:39 -0700

    Screens have been completed!!!

--- routes/apply.js
+++ routes/apply.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Apply page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { });
+  res.render('apply', { });
 });
 
 module.exports = router;
